7e35148a3e927753fa5a1413eb526a4ce3011cbf
Yago Mendes <119672907+DevMendes21@users.noreply.github.com>
2024-06-19 17:38:19 -0300

    Update sendMail_LucyHome.ts

--- sendMail_LucyHome.ts
+++ sendMail_LucyHome.ts
@@ -1,5 +1,5 @@
 function sendMail(formattedDate, error) {
-  const rep = 'yago.rodrigues@haaify.com'; 'ennisten.mudado@haaify.com';
+  const rep = 'yago.rodrigues@haaify.com';
   const sub = 'Erro teste';
   const message = `Ocorreu um na Planilha CTW (Lucy Home) erro na data ${formattedDate}.\n${error}`;
   GmailApp.sendEmail(rep, sub, message);
